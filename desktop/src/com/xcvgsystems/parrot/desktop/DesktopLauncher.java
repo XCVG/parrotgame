a181421263a45f84eb3da22bf13c28c739ae53c6
XCVG <xcvgvcx@gmail.com>
2016-07-11 12:07:47 -0700

    I was pretty sure I was using the title and stuff from the vars classes but apparently not, so now that's fixed

--- desktop/src/com/xcvgsystems/parrot/desktop/DesktopLauncher.java
+++ desktop/src/com/xcvgsystems/parrot/desktop/DesktopLauncher.java
@@ -2,11 +2,16 @@ package com.xcvgsystems.parrot.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.xcvgsystems.hypergiant.EVars;
+import com.xcvgsystems.hypergiant.GVars;
 import com.xcvgsystems.parrot.ParrotGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = EVars.WINDOW_X;
+		config.height = EVars.WINDOW_Y;
+		config.title = GVars.GAME_NAME;
 		new LwjglApplication(new ParrotGame(), config);
 	}
 }
